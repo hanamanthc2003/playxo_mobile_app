0e051cc978ad16b24ed1c084f8c97482be9bc8e9
Rachotesh <YOUR_EMAIL@gmail.com>
2026-06-23 14:35:28 +0530

    added splash screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,44 @@
-import { SafeAreaView } from "react-native";
+import { View, Image, ActivityIndicator } from "react-native";
 import { WebView } from "react-native-webview";
+import { useState } from "react";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ flex: 1 }}>
       <WebView
         source={{ uri: "https://playxo.app/" }}
         javaScriptEnabled
         domStorageEnabled
-        startInLoadingState
+        onLoadEnd={() => setLoading(false)}
+        style={{ flex: 1 }}
       />
-    </SafeAreaView>
+
+      {loading && (
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#fff",
+          }}
+        >
+          <Image
+            source={require(("../../assets/images/icon.png"))}
+            style={{ width: 120, height: 120 }}
+            resizeMode="contain"
+          />
+          <ActivityIndicator
+            size="large"
+            style={{ marginTop: 20 }}
+          />
+        </View>
+      )}
+    </View>
   );
 }
